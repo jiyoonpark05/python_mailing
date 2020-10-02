7008b9da8e274317915c909b5dd6e5dd5cf1fb42
jiyoonPark <jiyoonpark05@gmail.com>
2020-10-02 21:33:34 +0900

    [feat] smtp : sending excel file

--- news.py
+++ news.py
@@ -4,25 +4,29 @@ from openpyxl import Workbook
 
 driver = webdriver.Chrome('./chromedriver')
 
-url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=추석"
+url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=나훈아"
 
 driver.get(url)
 req = driver.page_source
 soup = BeautifulSoup(req, 'html.parser')
 
-articles = soup.select("#main_pack > div.news.mynews.section._prs_nws > ul> li")
-
 wb = Workbook()
 ws1 = wb.active
-ws1.title = 'articles'
-ws1.append(['제목','링크','신문사'])
+ws1.title = "practice_articles"
+ws1.append(['title','link','company','thumbnail'])
 
-for article in articles :
-    title = article.select_one('dl>dt>a').text
-    url = article.select_one('dl>dt>a')['href']
-    company = article.select_one("span._sp_each_source").text.split(' ')[0].replace('언론사','')
+articles = soup.select('#main_pack > div.news.mynews.section._prs_nws > ul> li')
 
-    ws1.append([title,url,company])
+for article in articles:
+    root = article.select_one('dl > dt > a')
+
+    title = article.select_one('dl > dt > a').text
+    url = article.select_one('dl > dt > a')['href']
+    company = article.select_one('dd.txt_inline > span._sp_each_source').text.split(' ')[0].replace('언론사','')
+    thumbnail = article.select_one('div > a > img')['src']
+
+    print([title,url,company,thumbnail])
+    ws1.append([title,url,company,thumbnail])
 
 driver.quit()
-wb.save(filename='articles.xlsx')
+wb.save(filename='practice_articles.xlsx')
